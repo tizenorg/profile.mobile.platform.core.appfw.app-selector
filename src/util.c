4957c7704c3700eadfb4f803a4480cbd0fbb6cb0
Jiwoong Im <jiwoong.im@samsung.com>
2015-12-01 20:43:55 +0900

    remove hard-coded enum value and extern declaration
    Change-Id: I34db298005862333dbc56e2cb1db293e951baf88
    Signed-off-by: Jiwoong Im <jiwoong.im@samsung.com>

--- src/util.c
+++ src/util.c
@@ -20,6 +20,7 @@
 #include <bundle.h>
 #include <bundle_internal.h>
 #include <aul.h>
+#include <aul_svc.h>
 #include <app_control.h>
 #include <aul_svc.h>
 #include <appsvc.h>
@@ -29,8 +30,6 @@ struct ucred;
 #include "util.h"
 #include "popup.h"
 #include "layout.h"
-extern int aul_forward_app(const char* pkgname, bundle *kb);
-extern int app_send_cmd_with_noreply(int pid, int cmd, bundle *kb);
 
 #define MAX_MIME_STR_SIZE 256
 
@@ -107,9 +106,8 @@ void _util_cancel(void *data)
 {
 	struct appdata *ad = (struct appdata *)data;
 	bundle *kb;
-	int ret_val;
+	int ret;
 	const char *pid = NULL;
-	char callee_pid[20]={0,};
 
 	pid = bundle_get_val(ad->kb, AUL_K_CALLER_PID);
 	if(pid == NULL) {
@@ -127,12 +125,10 @@ void _util_cancel(void *data)
 	bundle_add(kb, AUL_K_SEND_RESULT, "1");
 	bundle_add(kb, AUL_K_CALLER_PID, pid);
 
-	snprintf(callee_pid, 20, "%d", getpid());
-	bundle_add(kb, AUL_K_CALLEE_PID, (const char*)callee_pid);
+	ret = aul_svc_send_result(kb, AUL_SVC_RES_CANCEL);
 
-	ret_val = app_send_cmd_with_noreply(-2, 7, kb); // 7 is APP_CANCEL
-	if(ret_val != AUL_R_OK) {
-		_E("app_send_cmd error(%d)", ret_val);
+	if (ret != AUL_SVC_RET_OK) {
+		_E("aul_send_result error(%d)", ret);
 	}
 
 	bundle_free(kb);
